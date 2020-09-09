87692a08f08b8925755409a6bef51a824a8b00c8
Bruno Tarditi <brunotarditi@hotmail.com>
2020-09-09 15:04:30 -0300

    TP1 Colaboración de clases

---
+++ Programación-3/TP1-Colaboracion-entre-clases/src/EjercicioA/Principal.java
@@ -0,0 +1,64 @@
+package EjercicioA;
+
+/**
+ *
+ * @author Bruno Tarditi
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+         //CREAR OBJETOS
+        
+        Expediente ex1 = new Expediente();
+        ex1.setId(1);
+        ex1.setLetra("A");
+        ex1.setNumero(1);
+        ex1.setDescripcion("N/A");
+        ex1.setTipo("Clasificado");
+        ex1.setAmbito("Militar");
+        
+        Expediente ex2 = new Expediente();
+        ex1.setId(1);
+        ex1.setLetra("A");
+        ex1.setNumero(1);
+        ex1.setDescripcion("N/A");
+        ex1.setTipo("Clasificado");
+        ex1.setAmbito("Militar");
+        
+        Expediente ex3 = new Expediente();
+        ex1.setId(1);
+        ex1.setLetra("A");
+        ex1.setNumero(1);
+        ex1.setDescripcion("N/A");
+        ex1.setTipo("Clasificado");
+        ex1.setAmbito("Militar");
+                
+        Control c1 = new Control();
+        c1.setId(1);
+        c1.setDenominacion("Prueba");
+        c1.setEsObligatorio(true);
+        
+        EstadoControl ec1 = new EstadoControl();
+        ec1.setId(1);
+        ec1.setAprobado(true);
+        
+        
+        ex1.getControles().add(c1);        
+        c1.setEstadoControl(ec1);
+        
+        
+        //A.1
+        System.out.println(ex1.getCaratulaExpediente());
+        
+        //A.2
+        System.out.println(ex1.getControlesObligatorio());
+        
+        //A.3
+        
+        
+        //A.4
+        
+        
+    }
+    
+}
